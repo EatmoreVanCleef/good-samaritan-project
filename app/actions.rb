9047818f6bf699031b9a1dcb1c8fe28810dcee9d
Steve Y <steve.yorke@gmail.com>
2015-05-22 17:45:15 -0700

    Photo Upload Implemented

--- app/actions.rb
+++ app/actions.rb
@@ -8,27 +8,26 @@ get '/' do
   erb :index
 end
 
+get '/summary' do
+  erb :summary
+end
+
 get '/images/:filename' do
   # open the file using the :filename param
   # create a variable to send to the showfile.html.erb
   # in that file, spit out all meaningful data
-  image_path = Dir.glob(Dir.pwd + "/app/uploads/*.jpg").first
+  image_path = Dir.glob(Dir.pwd + "/public/uploads/*.jpg").first
   @image = Image.new(image_path)
   erb '/images/showfile'.to_sym
 end
-
-# Handle GET-request (Show the upload form)
-get "/upload" do
-  haml :upload
-end      
-    
+  
 # Handle POST-request (Receive and save the uploaded file)
 post "/upload" do 
-  File.open(Dir.pwd + '/app/uploads/' + params['myfile'][:filename], "w") do |f|
+  File.open(Dir.pwd + '/public/uploads/' + params['myfile'][:filename], "w") do |f|
     f.write(params['myfile'][:tempfile].read)
   end
-
-  redirect("/images/#{params['myfile'][:filename]}")
+  redirect "/summary"
+  # redirect("/images/#{params['myfile'][:filename]}")
 
 end
 
