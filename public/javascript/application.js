3dfbbeb08f6b1cc2f2136499cb88d14b8423af39
Steve Y <steve.yorke@gmail.com>
2015-05-23 20:15:13 -0700

    Implemented Basic Styling on Index and Summary

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-  $("#image-upload").change(function() {
-    $("#upload-form").submit();
-  });
-});
+// $(document).ready(function() {
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  
+//   $("#image-upload").change(function() {
+//     $("#upload-form").submit();
+//   });
+
+
+//   $(".btn").click(function(){
+//     $(this).button('loading').delay(1000).queue(function(){
+//       $(this).button('somestringvalue');
+//       $(this).dequeue();
+//     });        
+//   });  
+// });
