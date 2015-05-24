ba6f8de175133eac88abf40df698a961f2b9d8f5
Ramtin Radfar <ramtinr@gmail.com>
2015-05-24 00:51:45 +0000

    fixed upload bugs

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,23 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require_relative 'email_bot.rb'
 
 get '/' do
   erb :index
 end
-
-get '/summary' do
+  
+# Handle POST-request (Receive and save the uploaded file)
+post "/upload" do 
+  @filename = params['myfile'][:filename]
+  File.open(Dir.pwd + "/public/uploads/#{@filename}", 'w') do |f|
+    f.write(params['myfile'][:tempfile].read)
+  end
+  image_path = Dir.glob(Dir.pwd + "/public/uploads/#{@filename}").first
+  @image = Image.new(image_path)
+  @mayor = Mayor.where(["city = ?", @image.get_city]).first
   erb :summary
+  # redirect("/images/#{params['myfile'][:filename]}")
 end
 
 # get '/images/:filename' do
@@ -18,19 +28,16 @@ end
 #   @image = Image.new(image_path)
 #   erb '/images/showfile'.to_sym
 # end
-  
-# Handle POST-request (Receive and save the uploaded file)
-post "/upload" do 
-  File.open(Dir.pwd + '/public/uploads/' + params['myfile'][:filename], "w") do |f|
-    f.write(params['myfile'][:tempfile].read)
-  end
-  image_path = Dir.glob(Dir.pwd + "/public/uploads/*.jpg").first
-  @image = Image.new(image_path)
-  @mayor = Mayor.where(["city = ?", @image.get_city]).first
-  erb :summary
-  # redirect("/images/#{params['myfile'][:filename]}")
 
+get '/summary' do
+  erb :summary
 end
 
+post '/summary' do
+  redirect '/sent'
+end
 
-
+get "/sent" do
+  EmailBot.send_email
+  erb :sent
+end
